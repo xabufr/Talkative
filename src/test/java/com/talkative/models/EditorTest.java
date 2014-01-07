2a7021edcbd1933c28f34e16c7e801f528f101b0
Eptwalabha <damiengabrielle@gmail.com>
2014-01-07 10:21:09 +0100

    correction des tests sur Editor

--- src/test/java/com/talkative/models/EditorTest.java
+++ src/test/java/com/talkative/models/EditorTest.java
@@ -7,15 +7,21 @@ import org.apache.cxf.interceptor.LoggingInInterceptor;
 import org.apache.cxf.interceptor.LoggingOutInterceptor;
 import org.apache.cxf.jaxrs.client.ClientConfiguration;
 import org.apache.cxf.jaxrs.client.WebClient;
+import org.apache.openejb.jee.EjbJar;
 import org.apache.openejb.jee.WebApp;
 import org.apache.openejb.junit.ApplicationComposer;
 import org.apache.openejb.testing.Classes;
 import org.apache.openejb.testing.EnableServices;
 import org.apache.openejb.testing.Module;
+
 import static org.junit.Assert.*;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import com.talkative.repositories.UserRepository;
+import com.talkative.repositories.UserRepositoryHardcoded;
+import com.talkative.resource.RepositoryFactory;
 import com.talkative.resource.UsersResource;
 
 @RunWith(ApplicationComposer.class)
@@ -28,13 +34,20 @@ public class EditorTest {
 		return new WebApp().contextRoot("talkative");
 	}
 	
+	@Module
+	@Classes({RepositoryFactory.class, UserRepositoryHardcoded.class})
+	public EjbJar ejbjar() {
+		return new EjbJar();
+	}
+	
 	@Test
 	public void canReturnHTTPCodeWhenUserTriesToConnect() {
 		
 		WebClient webClient = this.createWebClient();
 		
 		try{
-			webClient.path("users/unknown").get(String.class);
+			webClient.path("users/unknown/").get(String.class);
+			fail("Exception expected");
 		}catch( Exception e ){
 			assertEquals(Status.NOT_FOUND.getStatusCode(), webClient.getResponse().getStatus());
 		}
@@ -49,10 +62,10 @@ public class EditorTest {
 		user.setEmail("toto@toto.fr");
 		user.setPassword("123456789");
 
-		webClient.path("users").post(user, Response.class);
+		webClient.path("users/").post(user, Response.class);
 		assertEquals(Status.CREATED.getStatusCode(), webClient.getResponse().getStatus());
 		
-		webClient.path("users").post("login=toto&password=12345678&email=toto@toto.fr", Response.class);
+		webClient.post(user, Response.class);
 		assertEquals(Status.CONFLICT.getStatusCode(), webClient.getResponse().getStatus());
 	}
 	
@@ -60,13 +73,17 @@ public class EditorTest {
 	public void returnsErrorWhileInscriptionIfLoginMissing() {
 		
 		WebClient webClient = this.createWebClient();
-		webClient.path("users").post("login=damien&password=12345678&email=toto@toto.fr", Response.class);
-		assertEquals(Status.CONFLICT.getStatusCode(), webClient.getResponse().getStatus());
+		User user = new User();
+		user.setEmail("toto@toto.fr");
+		user.setPassword("123456789");
+		webClient.path("users/").post(user, Response.class);
+		
+		assertEquals(Status.NOT_ACCEPTABLE.getStatusCode(), webClient.getResponse().getStatus());
 		
 	}
 	
 	private WebClient createWebClient() {
-        WebClient client = WebClient.create("http://localhost:4204/talkative"); // /api/");
+        WebClient client = WebClient.create("http://localhost:4204/talkative"); ///api/");
         ClientConfiguration config = WebClient.getConfig(client);
         config.getInInterceptors().add(new LoggingInInterceptor());
         config.getOutInterceptors().add(new LoggingOutInterceptor());
